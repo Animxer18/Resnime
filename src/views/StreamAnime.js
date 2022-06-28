bcc716fef7cab51361eec4fb8c6bee7357f5de07
=Restu Averian Putra <=restuaverianputra123@gmail.com>
2022-06-28 07:21:51 +0700

    add alert and fix font-size for hp

--- src/views/StreamAnime.js
+++ src/views/StreamAnime.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Loading from '../components/Loading';
 
@@ -54,7 +54,7 @@ function StreamAnime(props) {
                 
                 <div className='container mx-auto my-5 px-5 card flex space-y-20 flex-col'>
                     <div className='flex flex-col space-y-3'>
-                        <h1 className='first-letter:uppercase font-semibold text-4xl '>{episode.replaceAll("-"," ")}</h1>
+                        <h1 className='first-letter:uppercase font-semibold lg:text-4xl sm:text-3xl hp:text-2xl '>{episode.replaceAll("-"," ")}</h1>
                         <div className=' text-center py-3 bg-gray-200 flex justify-center items-start space-x-2 border border-gray-400 rounded-md '>
                             <div>
                                 <div className='flex space-x-5 items-center px-5 cursor-pointer ' onClick={()=>openHint()}>
@@ -65,13 +65,15 @@ function StreamAnime(props) {
                                     <i className={`bx bxs-chevron-down ${hiddenHint?"":"bx-rotate-180"} transition-all duration-300`}></i>
                                 </div>
                                 
-                                <div className={`overflow-hidden ${hiddenHint?"h-0":"lg:h-40 hp:h-56 hp:bg-blue-600 bg-red-500 hp:text-left h-44"} px-8 flex flex-col space-y-5 duration-300 transition-all`}>
+                                <div className={`overflow-hidden ${hiddenHint?"h-0":"lg:h-48 hp:h-[440px] sm:h-[200px] hp:text-left h-44"} px-8 flex flex-col space-y-5 duration-300 transition-all`}>
                                     <ul className='marker:text-gray-500 list-disc text-left my-4'>
-                                        <li>Nonton di PC/Laptop</li>
+                                        <li>Kalau buka di hp, mohon buka mode landscape atau auto-rotate</li>
+                                        <li>Kalau masih belum juga, coba untuk nonton di PC/Laptop</li>
                                         <li>Refresh / buka ulang websitenya</li>
                                         <li>Tutup devtools</li>
+                                        <li>Kalau masih belum juga, buka halaman <Link to="/not-found" className='font-bold'>ini</Link>, kasih tau di sana</li>
                                     </ul>
-                                    <strong>Maaf atas ketidaknyamanannya, terima kasih</strong>
+                                    <strong className='sm:text-center'>Maaf atas ketidaknyamanannya, terima kasih</strong>
                                 </div>
                             </div>
                         </div>
